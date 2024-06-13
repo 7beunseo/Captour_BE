2dd9805ed4850d600b2e802d1faa71d5f2a1846c
7beunseo <kimes0403@gmail.com>
2024-06-14 03:12:32 +0900

    프로젝트 생성

---
+++ src/test/java/com/mobileapp/Captour_BE/CaptourBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileapp.Captour_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptourBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
